9e57f1f94e7b45288da8c0f93e48f1db330ba545
Reyyyyyy <49629845+Reyyyyyy@users.noreply.github.com>
2020-08-09 21:59:51 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,127 +1,84 @@
-import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
+
+
 import os
-from sklearn.utils import shuffle
+import numpy as np
+from matplotlib import pyplot as plt
+from tensorflow.examples.tutorials.mnist import input_data
+from keras.layers import Input, Conv2D, MaxPooling2D, Flatten ,Dense ,Dropout ,GlobalAveragePooling2D
+from keras.models import Model,load_model
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.inception_v3 import InceptionV3,preprocess_input
+from keras.optimizers import Adagrad
+import keras
 
-#超参数
-lr = 0.0001
-batch_size = 24
-epochs = 3
-view_1 = 5
-view_2 = 5
-view_3 = 5
-view_4 = 5
-view_5 = 5
-num_filter_1 = 32
-num_filter_2 = 64
-num_filter_3 = 64
-num_filter_4 = 64
-num_filter_5 = 64
-fc_neuron_num = 1024
-use_dropout = True
-dropout = 0.5#每个元素被保留的概率
-keep_prob = tf.placeholder(tf.float32)
+#Loss =  0.0495    Train accuracy =  0.9835    Test accuracy = 0.988
 
-def get_data():
-    x_train = np.load(r'C:\Users\tensorflow\Desktop\迁移学习\dateset\train_batches.npy',allow_pickle=True)
-    y_train = np.load(r'C:\Users\tensorflow\Desktop\迁移学习\dateset\train_labels.npy',allow_pickle=True)
-    return x_train,y_train
+#采用增强的数据进行训练
+train_datagen = ImageDataGenerator(rotation_range = 40,
+                                   width_shift_range = 0.2,
+                                   height_shift_range = 0.2,
+                                   rescale = 1/255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True,
+                                   fill_mode = 'nearest')
+train = train_datagen.flow_from_directory(r'D:\kaggle\data\for_enhanced_data\train',
+                                    target_size=(299,299),
+                                    batch_size=96,
+                                    class_mode='binary')
+#创造测试集
+test_batches = (np.load(r'D:\kaggle\test_batches.npy',allow_pickle=True))/255
+test_labels = np.load(r'D:\kaggle\test_labels.npy',allow_pickle=True)
+#建模(迁移学习InceptionV3)
+base_model = InceptionV3(weights=None,include_top=False)
+base_model.load_weights(r'D:\kaggle\inceptionV3\inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5')
+# 增加新的输出层
+global_pooling = GlobalAveragePooling2D()(base_model.output) # GlobalAveragePooling2D 将 MxNxC 的张量转换成 1xC 张量，C是通道数
+fc = Dense(512,activation='relu')(global_pooling)
+drop = Dropout(rate=0.8)(fc)
+output = Dense(1,activation='sigmoid')(drop)
+model = Model(inputs=base_model.input,outputs=output)
+
+#编译模型(两步，第一步只训练新层，基础模型冻结；第二步训练基础模型的部分层以及新层，其余层冻结。
+def setup_to_transfer_learning(model,base_model):#base_model
+    for layer in base_model.layers:
+        layer.trainable = False
+    model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+
+def setup_to_fine_tuning(model,base_model):
+    FROZEN_LAYER = 16 
+    for layer in base_model.layers[:FROZEN_LAYER]:
+        layer.trainable = False
+    for layer in base_model.layers[FROZEN_LAYER:]:
+        layer.trainable = True
+    model.compile(optimizer=Adagrad(lr=0.0001),loss='binary_crossentropy',metrics=['accuracy'])
+
+#训练和评估模型
+def test_model(model,test_batches,test_labels,nums=500):
+    preds = (model.predict(test_batches[:nums])).flatten() > 0.5
+    labels = np.argmax(test_labels[:nums],axis=1)
+    return np.sum(preds == labels)/nums
     
-def conv2d(x,w,b,strides=1):
-    x = tf.nn.conv2d(x,w,strides=[1,strides,strides,1],padding='SAME')
-    x = tf.nn.bias_add(x,b)
+setup_to_transfer_learning(model,base_model)
+model.fit_generator(train,
+                    samples_per_epoch = 3000,
+                    nb_epoch = 3,
+                    #validation_data = validation_generator,
+                    #nb_val_samples=400)
+                    )
+print('Test Accuracy = ',test_model(model,test_batches,test_labels,nums=500))
+setup_to_fine_tuning(model,base_model)
+model.fit_generator(train,
+                    samples_per_epoch = 3000,
+                    nb_epoch = 3,
+                    #validation_data = validation_generator,
+                    #nb_val_samples=400)
+                    )
+print('Test Accuracy = ',test_model(model,test_batches,test_labels,nums=500))
 
-    return tf.nn.relu(x)
+#保存模型
+model.save_weights("weights")
+with open("model.json", "w") as f:
+    f.write(model.to_json())
 
-def maxpool2d(x,k=2):
-    x = tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,k,k,1],padding='SAME')
-    return x
-
-def conv_net(x,weights,biases,use_dropout):
-    
-    conv_1 = conv2d(x,weights['wc1'],biases['bc1'])
-    conv_1 = maxpool2d(conv_1)
-    
-    conv_2 = conv2d(conv_1,weights['wc2'],biases['bc2'])
-    
-    conv_3 = conv2d(conv_2,weights['wc3'],biases['bc3'])
-    conv_3 = maxpool2d(conv_3)
-    
-    conv_4 = conv2d(conv_3,weights['wc4'],biases['bc4'])
-    conv_4 = maxpool2d(conv_4)
-
-    conv_5 = conv2d(conv_4,weights['wc5'],biases['bc5'])
-    conv_5 = maxpool2d(conv_5)
-    
-    flatten = tf.reshape(conv_5,[-1,14*14*num_filter_5])
-
-    fc = tf.nn.relu(tf.matmul(flatten,weights['wf'])+biases['bf'])
-
-    if use_dropout:
-        fc = tf.nn.dropout(fc,keep_prob)
-
-    out = tf.matmul(fc,weights['out']) + biases['out']
-
-    return out
-
-weights={'wc1':tf.Variable(tf.random.truncated_normal([view_1,view_1,3,num_filter_1],stddev=0.02)),
-         'wc2':tf.Variable(tf.random.truncated_normal([view_2,view_2,num_filter_1,num_filter_2],stddev=0.02)/np.sqrt(num_filter_1/2)),
-         'wc3':tf.Variable(tf.random.truncated_normal([view_3,view_3,num_filter_2,num_filter_3],stddev=0.02)/np.sqrt(num_filter_2/2)),
-         'wc4':tf.Variable(tf.random.truncated_normal([view_4,view_4,num_filter_3,num_filter_4],stddev=0.05)/np.sqrt(num_filter_3/2)),
-         'wc5':tf.Variable(tf.random.truncated_normal([view_5,view_5,num_filter_4,num_filter_5],stddev=0.05)/np.sqrt(num_filter_4/2)),
-         'wf':tf.Variable(tf.random.truncated_normal([14*14*num_filter_5,fc_neuron_num],stddev=0.04)/np.sqrt(num_filter_3/2)),
-         'out':tf.Variable(tf.random.truncated_normal([fc_neuron_num,2],stddev=1/192)/np.sqrt(192/2))
-         }
-
-biases={'bc1':tf.Variable(tf.zeros([num_filter_1])),
-        'bc2':tf.Variable(tf.zeros([num_filter_2])),
-        'bc3':tf.Variable(tf.zeros([num_filter_3])),
-        'bc4':tf.Variable(tf.zeros([num_filter_4])),
-        'bc5':tf.Variable(tf.zeros([num_filter_5])),
-        'bf':tf.Variable(tf.zeros([fc_neuron_num])),
-        'out':tf.Variable(tf.zeros([2])),
-        }
-
-x = tf.placeholder(tf.float32,[None,224,224,3])
-y = tf.placeholder(tf.float32,[None,2])
-
-pred = conv_net(x,weights,biases,use_dropout)
-cost=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred,labels=y))
-optimizer = tf.train.AdamOptimizer(lr).minimize(cost)
-accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(pred,axis=1),tf.argmax(y,axis=1)),dtype=tf.float32))
-init = tf.global_variables_initializer()
-
-if __name__=='__main__':  
-    xs,ys = get_data()#变量名千万不能和占位符重名
-    train_x = xs[0:20000]
-    train_y = ys[0:20000]
-    test_x = xs[20000:25000]
-    test_y = ys[20000:25000]
-    with tf.Session() as sess:
-        sess.run(init)
-        #Train
-        steps = int(epochs*20000/batch_size)
-        for step in range(steps):
-            pointer = step*batch_size%20000
-            x_batch = train_x[pointer:pointer+batch_size]
-            y_batch = train_y[pointer:pointer+batch_size]
-
-            sess.run(optimizer,feed_dict={x:x_batch,y:y_batch,keep_prob:dropout})
-            loss = sess.run(cost,feed_dict={x:x_batch,y:y_batch,keep_prob:dropout})
-            acc = sess.run(accuracy,feed_dict={x:x_batch,y:y_batch,keep_prob:1.0})
-            print('loss:',loss)
-            print('accuracy:',acc,'\n')
-            
-        #Evaluate
-        avg_acc_test = 0
-        for i in range(50):
-            acc_test = sess.run(accuracy,feed_dict={x:test_x[i*100:(i+1)*100],y:test_y[i*100:(i+1)*100],keep_prob:1.0})
-            print('Test accuracy: ',acc_test)
-            avg_acc_test += acc_test
-
-        avg_acc_test = avg_acc_test/50
-        print('Done! Average accuracy of test data is: ',avg_acc_test)
-        #Save
-        saver.save(sess,'my_model.ckpt')
+input()
