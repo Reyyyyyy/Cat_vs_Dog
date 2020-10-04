e9ad384f21720e1377b632b107f6162387c21208
Reyyyyyy <49629845+Reyyyyyy@users.noreply.github.com>
2020-10-04 23:30:53 +0800

    Delete preview_vgg16.py

--- preview_vgg16.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-vgg16 = (np.load('vgg16.npy',allow_pickle=True,encoding='bytes')).tolist()
-for name,params in vgg16.items():
-    weight = params[0]
-    bias = params[1]
-    print('name:',str(name).replace('b','').replace('\'',''),\
-          '  weights\'shape:',str(weight.shape),\
-          '  bias\'shape',str(bias.shape))
